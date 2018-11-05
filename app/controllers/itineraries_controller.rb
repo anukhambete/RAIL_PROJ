09afa3b1457def3465bef416885a18231aa57c87
Anu Khambete <anukhambete@gmail.com>
2018-11-05 04:26:04 +0000

    Itinerary show template

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,6 +1,41 @@
 class ItinerariesController < ApplicationController
 
   def index
+    @itineraries = Itinerary.all
   end
-  
+
+  def new
+    @itinerary = Itinerary.new
+    @user = User.find(session[:user_id])
+  end
+
+  def create
+    #binding.pry
+    @itinerary = Itinerary.new(itinerary_params, params[:user_id])
+    if @itinerary.save
+      @itinerary.user = User.find(session[:user_id])
+      @itinerary.save
+      #binding.pry
+      redirect_to itineraries_path
+    else
+      @user = User.find(session[:user_id])
+      render :new
+    end
+  end
+
+  def show
+    #binding.pry
+    @itinerary = Itinerary.find(params[:id])
+  end
+
+  private
+
+  def itinerary_params
+    params.require(:itinerary).permit(:name, :description)
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
 end
--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,4 +1,11 @@
 class Itinerary < ActiveRecord::Base
   has_many :places
   belongs_to :user
+  accepts_nested_attributes_for :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  #validates :password, presence: true
+
+  validates_uniqueness_of :name
 end
