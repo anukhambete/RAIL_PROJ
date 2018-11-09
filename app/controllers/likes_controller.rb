e294222e54f58a7d7918d5e18126dcbf65f173db
Anu Khambete <anukhambete@gmail.com>
2018-11-09 17:24:22 +0000

    Add like to show page

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,11 @@
 require 'pry'
 class LikesController < ApplicationController
   def create
-    binding.pry
+    #binding.pry
+    @itinerary = Itinerary.find(params[:itinerary_id])
+    @like = Like.find_or_create_by(itinerary_id: @itinerary.id, user_id: current_user.id)
+    redirect_to itinerary_path(@itinerary)
+    #binding.pry
   end
 
   def update
@@ -11,4 +15,5 @@ class LikesController < ApplicationController
   def current_user
     User.find(session[:user_id])
   end
+
 end
