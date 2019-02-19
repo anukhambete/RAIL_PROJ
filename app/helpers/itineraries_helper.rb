4ffa3e2d8881d7aad66d196a0440651f82cb1a47
github username <github email address>
2019-02-19 16:33:29 -0500

    add ratings helper

--- app/helpers/itineraries_helper.rb
+++ app/helpers/itineraries_helper.rb
@@ -6,9 +6,22 @@ def current_user_likes_itins(user, itinerary)
     arr << like.itinerary_id
     arr
   end
-  arr.include?(itinerary.id) ? true : false  
+  arr.include?(itinerary.id) ? true : false
 end
 
+def ratings_average(itinerary)
+  @itinerary = itinerary
 
+  arr =[]
+  if !@itinerary.likes.empty?
+    @itinerary.likes.each do |like|
+      arr << like.rating
+    end
+    avg = arr.sum/arr.length
+  else
+    avg = ""
+  end
+
+end
 
 end
